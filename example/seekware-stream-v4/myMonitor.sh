296c0eb1d931a4f9ebfeb3952d4d1e334c508680
younlea kim <younlea.kim@gmail.com>
2020-09-12 21:47:32 +0900

    add auto check and re run about seekware-stream

--- example/seekware-stream-v4/myMonitor.sh
+++ example/seekware-stream-v4/myMonitor.sh
@@ -2,15 +2,30 @@
 
 while [ 1 ]
     do
-        Cnt=`ps -ex|grep "seekware-tcpip"|grep -v grep|wc -l`
-        PROCESS=`ps -ex|grep "seekware-tcpip"|grep -v grep|awk '{print $1}'`
-    if [ $Cnt -ne 0 ]
+        Cnt1=`ps -aux|grep "seekware-tcpip"|grep -v grep|wc -l`
+
+        Cnt2=`ps -aux|grep "seekware-stream"|grep -v grep|wc -l`
+
+        PROCESS1=`ps -aux|grep "seekware-tcpip"|grep -v grep|awk '{print $1}'`
+
+        PROCESS2=`ps -aux|grep "seekware-stream"|grep -v grep|awk '{print $1}'`
+
+    if [ $Cnt1 -ne 0 ]
         then
-            echo "seekware-tcpip (PID : $PROCESS) alredy runing"
+            echo "seekware-tcpip (PID : $PROCESS1) alredy runing"
     else
         seekware-tcpip &
         echo "seekware-tcpip run"
-fi
+    fi
+
+    if [ $Cnt2 -ne 0 ]
+        then
+            echo "seekware-stream (PID : $PROCESS2) alredy runing"
+    else
+        seekware-stream &
+        echo "seekware-stream run"
+    fi
+
     sleep 3
 done
 
