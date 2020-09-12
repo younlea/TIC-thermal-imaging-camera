80039820570e0de8cd5efee500083d8bc10d1895
younlea kim <younlea.kim@gmail.com>
2020-09-12 22:48:53 +0900

    change auto run order

--- example/seekware-stream-v4/myMonitor.sh
+++ example/seekware-stream-v4/myMonitor.sh
@@ -2,25 +2,25 @@
 
 while [ 1 ]
     do
-        Cnt1=`ps -aux|grep "seekware-tcpip"|grep -v grep|wc -l`
+        Cnt2=`ps -aux|grep "seekware-tcpip"|grep -v grep|wc -l`
 
-        Cnt2=`ps -aux|grep "seekware-stream"|grep -v grep|wc -l`
+        Cnt1=`ps -aux|grep "seekware-stream"|grep -v grep|wc -l`
 
-        PROCESS1=`ps -aux|grep "seekware-tcpip"|grep -v grep|awk '{print $1}'`
+        PROCESS2=`ps -aux|grep "seekware-tcpip"|grep -v grep|awk '{print $1}'`
 
-        PROCESS2=`ps -aux|grep "seekware-stream"|grep -v grep|awk '{print $1}'`
+        PROCESS1=`ps -aux|grep "seekware-stream"|grep -v grep|awk '{print $1}'`
 
-    if [ $Cnt1 -ne 0 ]
+    if [ $Cnt2 -ne 0 ]
         then
-            echo "seekware-tcpip (PID : $PROCESS1) alredy runing"
+            echo "seekware-tcpip (PID : $PROCESS2) alredy runing"
     else
         seekware-tcpip &
         echo "seekware-tcpip run"
     fi
 
-    if [ $Cnt2 -ne 0 ]
+    if [ $Cnt1 -ne 0 ]
         then
-            echo "seekware-stream (PID : $PROCESS2) alredy runing"
+            echo "seekware-stream (PID : $PROCESS1) alredy runing"
     else
         seekware-stream &
         echo "seekware-stream run"
