c203ce66e739cb502c3cb09b8fe1846b05729d57
younlea kim <younlea.kim@gmail.com>
2020-11-11 05:36:42 +0900

    add auto wb setting on raspi camera

--- example/seekware-stream-v4/streaming.sh
+++ example/seekware-stream-v4/streaming.sh
@@ -2,6 +2,6 @@
 cvlc v4l2:// :v4l2-dev=/dev/video4 --sout="#transcode{vcodec=h264,venc=x264{preset=ultrafast,tune=zerolatency,intra-refresh,lookahead=10,keyint=15},acodec=mpga,ab=128,channels=2,samplerate=44100,scodec=none}:rtp{sdp=rtsp://:8554/seekThermal}" &
 #ffmpeg -f x11grab -s 320x240 -preset ultrafast -tune zerolatency -r 50 -vcodec mpeg2video -b:v 8000 -f rtp rtp://:8554/seekThermal &
 
-raspivid -o - -t 0 -w 640 -h 360 -fps 25 | cvlc -vvv stream:///dev/stdin --sout '#standard{access=http, mux=ts, dst=:8090}' :demux=h264 &
+raspivid -o - -ex auto -awb auto -ISO 400 -t 0 -w 640 -h 360 -fps 25 | cvlc -vvv stream:///dev/stdin --sout '#standard{access=http, mux=ts, dst=:8090}' :demux=h264 &
 
 
